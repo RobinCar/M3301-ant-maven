51028fde77c1ffd8e24e2c68dcc846fe35fc01fd
AllanC56 <coleo.plafond@gmail.com>
2017-09-20 11:18:51 +0200

    Added JSON writing

--- src/main/java/jwallet/ui/JWalletFrame.java
+++ src/main/java/jwallet/ui/JWalletFrame.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package jwallet.ui;
 
 import java.util.Arrays;
@@ -12,26 +12,26 @@ import java.util.Vector;
 import javax.swing.table.DefaultTableModel;
 
 /**
- *
- * @author nicolas
- */
+*
+* @author nicolas
+*/
 public class JWalletFrame extends javax.swing.JFrame {
 
-  private javax.swing.JMenuItem addMenuItem;
-  private javax.swing.JMenu editMenu;
-  private javax.swing.JMenu fileMenu;
-  private javax.swing.JScrollPane jScrollPane1;
-  private javax.swing.JMenuBar menuBar;
-  private javax.swing.JMenuItem quitMenuItem;
-  private javax.swing.JMenuItem removeMenuItem;
-  private javax.swing.JMenuItem updateMenuItem;
-  private javax.swing.JTable table;
-  private javax.swing.table.DefaultTableModel model;
-  private Vector<String> data;
+    private javax.swing.JMenuItem addMenuItem;
+    private javax.swing.JMenu editMenu;
+    private javax.swing.JMenu fileMenu;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JMenuBar menuBar;
+    private javax.swing.JMenuItem quitMenuItem;
+    private javax.swing.JMenuItem removeMenuItem;
+    private javax.swing.JMenuItem updateMenuItem;
+    private javax.swing.JTable table;
+    private javax.swing.table.DefaultTableModel model;
+    private Vector<String> data;
 
     /**
-     * Creates new form MainFrame
-     */
+    * Creates new form MainFrame
+    */
     public JWalletFrame() {
         initComponents();
     }
@@ -84,15 +84,15 @@ public class JWalletFrame extends javax.swing.JFrame {
         editMenu.add(addMenuItem);
 
         updateMenuItem.setText("Modifier");
-		updateMenuItem.addActionListener(new java.awt.event.ActionListener() {
+        updateMenuItem.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 updateMenuItemActionPerformed(evt);
             }
         });
         editMenu.add(updateMenuItem);
 
-		removeMenuItem.setText("Supprimer");
-		removeMenuItem.addActionListener(new java.awt.event.ActionListener() {
+        removeMenuItem.setText("Supprimer");
+        removeMenuItem.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 removeMenuItemActionPerformed(evt);
             }
@@ -106,12 +106,12 @@ public class JWalletFrame extends javax.swing.JFrame {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE)
+        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE)
         );
 
         pack();
@@ -126,13 +126,25 @@ public class JWalletFrame extends javax.swing.JFrame {
     }
 
 
-	private void updateMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
-
+    private void updateMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
+        for (int i = 0; i < this.table.getRowCount(); i++) {
+            JSONObject obj = new JSONObject();
+            obj.put("URL", this.table.getRow(i).getColumn(0).getValue());
+            obj.put("Identifiant", this.table.getRow(i).getColumn(1).getValue());
+            obj.put("Mot de passe", this.table.getRow(i).getColumn(2).getValue());
+            obj.put("Commentaire", this.table.getRow(i).getColumn(3).getValue());
+            try (FileWriter file = new FileWriter(".")) {
+                file.write(obj.toJSONString());
+                file.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-	private void removeMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
-            int selectedRow = table.getSelectedRow();
-            model.removeRow(selectedRow);
+    private void removeMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
+        int selectedRow = table.getSelectedRow();
+        model.removeRow(selectedRow);
     }
 
     private void quitMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
